d9205551ed20df948c861cd28ef086bf8a9f2518
sagostinelli <batonenbois@gmail.com>
2021-02-07 17:10:40 -0500

    part1 code

---
+++ src/components/MyProfile/index.js
@@ -0,0 +1,12 @@
+const MyProfile = () => {
+
+  return (
+    <div className="my-class">
+      <h1>MyProfile</h1>
+      <p>Name: Codelimbing</p>
+      <p>Description: Best youtube channel ever!</p>
+    </div>
+  );
+};
+
+export default MyProfile;
